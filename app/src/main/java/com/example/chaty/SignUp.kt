bcb1f946e133225264460a191e2f0e57b6957378
Hrvooje <cuckovichrvoje@gmail.com>
2023-05-20 20:05:29 +0200

    19:52

--- app/src/main/java/com/example/chaty/SignUp.kt
+++ app/src/main/java/com/example/chaty/SignUp.kt
@@ -1,11 +1,32 @@
 package com.example.chaty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var edtName:EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        edtName=findViewById(R.id.edt_name)
+        edtEmail=findViewById(R.id.edt_email)
+        edtPassword=findViewById(R.id.edt_password)
+        btnSignUp=findViewById(R.id.btn_signUp)
+        btnLogin=findViewById(R.id.btn_logIn)
+
+        btnLogin.setOnClickListener {
+            val intent= Intent(this,LogIn::class.java)
+            startActivity(intent)
+        }
     }
 }
